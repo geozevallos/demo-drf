6cd58c9cb6f954733188e779fde4db4c953bd009
Geojorge <jorgezevallosr@gmail.com>
2024-08-24 00:02:33 -0500

    Usando decorador ApiView

--- demo/myapp/serializers.py
+++ demo/myapp/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from myapp.models import Toy
 
+"""
 class ToySerializer(serializers.Serializer):
     pk = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=150)
@@ -21,3 +22,14 @@ class ToySerializer(serializers.Serializer):
         instance.was_included_in_home = validated_data.get('was_included_in_home', instance.was_included_in_home)
         instance.save()
         return instance
+"""
+
+  
+class ToySerializer(serializers.ModelSerializer):
+        class Meta:
+            model = Toy
+            fields = ['id', 'name','description','release_date',
+                      'toy_category','was_included_in_home']
+    
+    
+    
